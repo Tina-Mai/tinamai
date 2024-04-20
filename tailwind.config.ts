f1345eccbca6e2521f52d41b8b855ca944291c09
tinamai <tinabmai@gmail.com>
2024-04-20 16:33:36 -0400

    first commit

---
+++ tailwind.config.ts
@@ -0,0 +1,30 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                dark: "#1E1E1E",
+            },
+            fontFamily: {
+                serif: ["Playfair Display", "serif"],
+                mono: ["JetBrains Mono", "monospace"],
+            },
+        },
+        screens: {
+            xs: "480px",
+            ss: "620px",
+            sm: "768px",
+            md: "1060px",
+            lg: "1200px",
+            xl: "1700px",
+        },
+    },
+    plugins: [],
+};
+export default config;
