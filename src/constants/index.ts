8bb6ff01115700b2011c579ea8bb08f0cdb58ebe
tinamai <tinabmai@gmail.com>
2024-04-21 11:47:47 -0400

    past items modularized

---
+++ src/constants/index.ts
@@ -0,0 +1,22 @@
+export const pastItems = [
+    {
+        id: '01',
+        description: 'won World Series of Innovation for making fintech software for the unbanked @ 15',
+    },
+    {
+        id: '02',
+        description: 'built NLP-based voice accessible app to help visually-impaired youth, awarded by U.S. Congress @ 16',
+    },
+    {
+        id: '03',
+        description: 'published in the New York Times @ 16, youngest recipient of the Atlanta Review International Poetry Award, + some other awards for writing',
+    },
+    {
+        id: '04',
+        description: 'spoke at SXSW @ 17 about building a nuclear fusion reactor in the garage',
+    },
+    {
+        id: '05',
+        description: "followed my parent's american dream of me going to a good college. made lifelong friends there. left after 10 weeks to chase a dream.",
+    },
+];
