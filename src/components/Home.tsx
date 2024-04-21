483a7a79385a2388d50ce57ec8bba4e204d55d2a
tinamai <tinabmai@gmail.com>
2024-04-21 01:06:21 -0400

    more links added

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -53,7 +53,17 @@ export default function Home() {
                             <div className='space-y-5'>
                                 <p>
                                     <p className='text-slate-500'>01</p>
-                                    <p className='pb-5'>co-founder @ Kndrd, engineering the social fabric that once only existed in institutions</p>
+                                    <p className='pb-5'>
+                                        <p className='inline'>co-founder @</p>{' '}
+                                        <div className='inline-flex items-center whitespace-nowrap'>
+                                            <a href='https://kndrd.com' target='_blank' rel='noopener noreferrer' className={`flex items-center ${styles.hoverEffect}`}>
+                                                <p className='mr-1'>Kndrd</p>
+                                                <ArrowUpRightIcon className='h-3.5 w-3.5' />
+                                            </a>
+                                            <p className='inline'>,</p>
+                                        </div>{' '}
+                                        <p className='inline'>engineering the social fabric that once only existed in institutions</p>
+                                    </p>
                                     <div className='pb-5 items-center flex-row gap-1'>
                                         <p className='inline'>featured 2x in</p>{' '}
                                         <a href='https://kndrd.com/press' target='_blank' rel='noopener noreferrer' className={`inline items-center flex-row gap-1 space-x-1 ${styles.hoverEffect}`}>
