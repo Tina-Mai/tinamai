fa0fceb88eb8710a2d932dcfc85b2e86ec0ce8ee
Tina <tinabmai@gmail.com>
2024-06-23 13:08:39 -0400

    don't mind me just being a indecisive with metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,82 +5,82 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 const title = 'Tina Mai';
 const description =
-    'Tina Mai is an 18-year-old startup founder and developer building in the machine learning space. She was recognized for creating an AI accessibility app for youth, a fintech software that won the World Series of Innovation, and more. She studies computer science and product design at Stanford.';
+	'Tina Mai is an 18-year-old startup founder and developer building in the machine learning space. She was recognized for creating an AI accessibility app for youth, a fintech software that won the World Series of Innovation, and more. She studies computer science and product design at Stanford.';
 
 export const metadata: Metadata = {
-    title: title,
-    description: description,
-    keywords: ['tina mai', 'tina', 'mai', 'kndrd', 'stanford', 'startup', 'founder', 'design', 'engineer', 'builder', 'machine learning', 'ai', 'computer science', 'product design'],
-    openGraph: {
-        title: title,
-        description: description,
-        siteName: title,
-        type: 'website',
-        locale: 'en_US',
-        images: [
-            {
-                url: '/opengraph-image.jpg',
-                width: 790,
-                height: 987,
-                alt: 'Tina Mai',
-            },
-        ],
-        url: 'https://tinamai.xyz',
-    },
-    twitter: {
-        title: title,
-        description: description,
-        images: [
-            {
-                url: '/twitter-image.jpg',
-                width: 600,
-                height: 600,
-                alt: 'Tina Mai',
-            },
-        ],
-        // use 'summary' for small image (e.g. profile pic), 'summary_large_image' for large
-        card: 'summary',
-    },
-    robots: {
-        index: true,
-        follow: true,
-        nocache: true,
-        googleBot: {
-            index: true,
-            follow: false,
-            noimageindex: true,
-            'max-video-preview': -1,
-            'max-image-preview': 'large',
-            'max-snippet': -1,
-        },
-    },
-    applicationName: title,
-    icons: {
-        icon: [
-            {
-                url: '/favicon.ico',
-                type: 'image/x-icon',
-            },
-        ],
-        shortcut: [
-            {
-                url: '/favicon.ico',
-                type: 'image/x-icon',
-            },
-        ],
-    },
-    metadataBase: new URL('https://tinamai.xyz'),
-    alternates: { canonical: 'https://tinamai.xyz' },
+	title: title,
+	description: description,
+	keywords: ['tina mai', 'tina', 'mai', 'kndrd', 'stanford', 'startup', 'founder', 'design', 'engineer', 'builder', 'machine learning', 'ai', 'computer science', 'product design'],
+	openGraph: {
+		title: title,
+		description: description,
+		siteName: title,
+		type: 'website',
+		locale: 'en_US',
+		images: [
+			{
+				url: '/opengraph-image.jpg',
+				width: 790,
+				height: 987,
+				alt: 'Tina Mai',
+			},
+		],
+		url: 'https://tinamai.xyz',
+	},
+	twitter: {
+		title: title,
+		description: description,
+		images: [
+			{
+				url: '/twitter-image.jpg',
+				width: 1200,
+				height: 600,
+				alt: 'Tina Mai',
+			},
+		],
+		// use 'summary' for small image (e.g. profile pic), 'summary_large_image' for large
+		card: 'summary_large_image',
+	},
+	robots: {
+		index: true,
+		follow: true,
+		nocache: true,
+		googleBot: {
+			index: true,
+			follow: false,
+			noimageindex: true,
+			'max-video-preview': -1,
+			'max-image-preview': 'large',
+			'max-snippet': -1,
+		},
+	},
+	applicationName: title,
+	icons: {
+		icon: [
+			{
+				url: '/favicon.ico',
+				type: 'image/x-icon',
+			},
+		],
+		shortcut: [
+			{
+				url: '/favicon.ico',
+				type: 'image/x-icon',
+			},
+		],
+	},
+	metadataBase: new URL('https://tinamai.xyz'),
+	alternates: { canonical: 'https://tinamai.xyz' },
 };
 
 export default function RootLayout({
-    children,
+	children,
 }: Readonly<{
-    children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-    return (
-        <html lang='en'>
-            <body className={inter.className}>{children}</body>
-        </html>
-    );
+	return (
+		<html lang='en'>
+			<body className={inter.className}>{children}</body>
+		</html>
+	);
 }
--- src/components/Home/Current/CurrentList.tsx
+++ src/components/Home/Current/CurrentList.tsx
@@ -5,42 +5,42 @@ import ScrambleText from '../../Global/ScrambleText';
 import { ArrowUpRightIcon } from '@heroicons/react/24/solid';
 
 const Item1 = () => (
-    <motion.li variants={animations.itemVariants}>
-        <p className='text-slate-500'>01</p>
-        <div className='pb-5'>
-            <p className='inline'>co-founder @</p>{' '}
-            <div className='inline-flex items-center whitespace-nowrap'>
-                <a href='https://kndrd.com' target='_blank' rel='noopener noreferrer' className='flex items-center link'>
-                    <ScrambleText text='Kndrd' className='mr-1' />
-                    <ArrowUpRightIcon className='h-3.5 w-3.5' />
-                </a>
-                <p className='inline'>,</p>
-            </div>{' '}
-            <p className='inline'>engineering the social fabric that once only existed in institutions</p>
-        </div>
-        <div className='pb-5 items-center flex-row gap-1'>
-            <p className='inline'>featured twice in</p>{' '}
-            <a href='https://kndrd.com/press' target='_blank' rel='noopener noreferrer' className='inline items-center flex-row gap-1 space-x-1 link'>
-                <ScrambleText text='Business Insider' className='inline' />
-                <ArrowUpRightIcon className='inline wrap h-3.5 w-3.5' />
-            </a>
-        </div>
-        <p>won Stanford&apos;s oldest & largest startup competition in consumer category</p>
-    </motion.li>
+	<motion.li variants={animations.itemVariants}>
+		<p className='text-slate-500'>01</p>
+		<div className='pb-5'>
+			<p className='inline'>co-founder @</p>{' '}
+			<div className='inline-flex items-center whitespace-nowrap'>
+				<a href='https://kndrd.com' target='_blank' rel='noopener noreferrer' className='flex items-center link'>
+					<ScrambleText text='Kndrd' className='mr-1' />
+					<ArrowUpRightIcon className='h-3.5 w-3.5' />
+				</a>
+				<p className='inline'>,</p>
+			</div>{' '}
+			<p className='inline'>engineering the social fabric that once only existed in institutions</p>
+		</div>
+		<div className='pb-5 items-center flex-row gap-1'>
+			<p className='inline'>featured twice in</p>{' '}
+			<a href='https://kndrd.com/press' target='_blank' rel='noopener noreferrer' className='inline items-center flex-row gap-1 space-x-1 link'>
+				<ScrambleText text='Business Insider' className='inline' />
+				<ArrowUpRightIcon className='inline wrap h-3.5 w-3.5' />
+			</a>
+		</div>
+		<p>won Stanford&apos;s oldest & largest startup competition in consumer category</p>
+	</motion.li>
 );
 
 const Item2 = () => (
-    <motion.li variants={animations.itemVariants}>
-        <p className='text-slate-500'>02</p>
-        studying CS & design engineering @ Stanford (on a leave of absence)
-    </motion.li>
+	<motion.li variants={animations.itemVariants}>
+		<p className='text-slate-500'>02</p>
+		studying CS & design engineering @ Stanford (on a leave of absence)
+	</motion.li>
 );
 
 const Item3 = () => (
-    <motion.li variants={animations.itemVariants}>
-        <p className='text-slate-500'>03</p>
-        sometimes i write essays. other times i think about writing them and mourn that i don&apos;t have much time to write anymore.
-    </motion.li>
+	<motion.li variants={animations.itemVariants}>
+		<p className='text-slate-500'>03</p>
+		sometimes i write essays. other times i think about writing them and mourn that i don&apos;t have much time to write anymore.
+	</motion.li>
 );
 
 const CurrentList = [Item1, Item2, Item3];
