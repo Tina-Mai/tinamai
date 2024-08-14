4078305e3d9837c845d0ff249e6378d2cd0e0a11
Tina Mai <tinabmai@gmail.com>
2024-08-14 13:32:46 -0700

    moved to prev

--- src/components/Home/Current/CurrentList.tsx
+++ src/components/Home/Current/CurrentList.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { animations } from "@/constants";
-import ScrambleText from "../../Global/ScrambleText";
-import { ArrowUpRightIcon } from "@heroicons/react/24/solid";
 import Link from "../../Global/Link";
 
 const Item1 = () => (
@@ -22,46 +20,17 @@ const Item2 = () => (
 const Item3 = () => (
 	<motion.li variants={animations.itemVariants}>
 		<p className="text-slate-500">03</p>
-		<div className="pb-5">
-			<p className="inline">co-founder/CTO @</p>{" "}
-			<div className="inline-flex items-center whitespace-nowrap">
-				<a href="https://kndrd.com" target="_blank" rel="noopener noreferrer" className="flex items-center link">
-					<ScrambleText text="Kndrd" className="mr-1" />
-					<ArrowUpRightIcon className="h-3.5 w-3.5" />
-				</a>
-				<p className="inline">,</p>
-			</div>{" "}
-			<p className="inline">engineering on-demand social</p>
-		</div>
-
-		<div>
-			<p className="inline">featured twice in</p>{" "}
-			<div className="inline-flex items-center whitespace-nowrap">
-				<a href="https://kndrd.com/press" target="_blank" rel="noopener noreferrer" className="flex items-center link">
-					<ScrambleText text="Business Insider" className="mr-1" />
-					<ArrowUpRightIcon className="h-3.5 w-3.5" />
-				</a>
-				<p className="inline">,</p>
-			</div>{" "}
-			<p className="inline">backed by Cal Henderson (Slack), Matt Higgins (Shark Tank), etc.</p>
-		</div>
+		venture partner @ <Link text="Contrary" href="https://contrary.com" />
 	</motion.li>
 );
 
 const Item4 = () => (
 	<motion.li variants={animations.itemVariants}>
 		<p className="text-slate-500">04</p>
-		venture partner @ <Link text="Contrary" href="https://contrary.com" />
-	</motion.li>
-);
-
-const Item5 = () => (
-	<motion.li variants={animations.itemVariants}>
-		<p className="text-slate-500">05</p>
 		sometimes i write essays. other times i think about writing them and mourn that i don&apos;t have much time to write anymore.
 	</motion.li>
 );
 
-const CurrentList = [Item1, Item2, Item3, Item4, Item5];
+const CurrentList = [Item1, Item2, Item3, Item4];
 
 export default CurrentList;
--- src/components/Home/Past/PastList.tsx
+++ src/components/Home/Past/PastList.tsx
@@ -2,23 +2,45 @@ import React from "react";
 import { motion } from "framer-motion";
 import { animations } from "@/constants";
 import Link from "../../Global/Link";
+import ScrambleText from "@/components/Global/ScrambleText";
+import { ArrowUpRightIcon } from "@heroicons/react/24/solid";
 
 const Item1 = () => (
 	<motion.li variants={animations.itemVariants}>
 		<p className="text-slate-500">01</p>
+		<div className="pb-5">
+			<p className="inline">co-founder/CTO @</p>{" "}
+			<div className="inline-flex items-center whitespace-nowrap">
+				<a href="https://kndrd.com" target="_blank" rel="noopener noreferrer" className="flex items-center link">
+					<ScrambleText text="Kndrd" className="mr-1" />
+					<ArrowUpRightIcon className="h-3.5 w-3.5" />
+				</a>
+				<p className="inline">,</p>
+			</div>{" "}
+			<p className="inline">engineering on-demand social</p>
+		</div>
+
 		<div>
-			<p className="inline">won</p> <Link text="World Series of Innovation" href="https://www.smes.org/news-detail?pk=1171095" />{" "}
-			<p className="inline">for making fintech software for the unbanked @ 15</p>
+			<p className="inline">featured twice in</p>{" "}
+			<div className="inline-flex items-center whitespace-nowrap">
+				<a href="https://kndrd.com/press" target="_blank" rel="noopener noreferrer" className="flex items-center link">
+					<ScrambleText text="Business Insider" className="mr-1" />
+					<ArrowUpRightIcon className="h-3.5 w-3.5" />
+				</a>
+				<p className="inline">,</p>
+			</div>{" "}
+			<p className="inline">backed by Cal Henderson (Slack), Matt Higgins (Shark Tank), etc.</p>
 		</div>
 	</motion.li>
 );
+
 
 const Item2 = () => (
 	<motion.li variants={animations.itemVariants}>
 		<p className="text-slate-500">02</p>
 		<div>
-			<p className="inline">built NLP-based voice accessible app for visually-impaired youth, awarded by</p>{" "}
-			<Link text="U.S. Congress" href="https://youtu.be/AqG4mt9EUyc?si=l8qvlhRPBfF78K-Z&t=1593" /> <p className="inline">@ 16</p>
+			<p className="inline">won</p> <Link text="World Series of Innovation" href="https://www.smes.org/news-detail?pk=1171095" />{" "}
+			<p className="inline">for making fintech software for the unbanked @ 15</p>
 		</div>
 	</motion.li>
 );
@@ -27,8 +49,8 @@ const Item3 = () => (
 	<motion.li variants={animations.itemVariants}>
 		<p className="text-slate-500">03</p>
 		<div>
-			<p className="inline">published in the</p> <Link text="New York Times" href="https://x.com/AppleFilms/status/1494089759546744832" />{" "}
-			<p className="inline">@ 16, youngest recipient of the Atlanta Review International Poetry Award, + some other awards for writing</p>
+			<p className="inline">built NLP-based voice accessible app for visually-impaired youth, awarded by</p>{" "}
+			<Link text="U.S. Congress" href="https://youtu.be/AqG4mt9EUyc?si=l8qvlhRPBfF78K-Z&t=1593" /> <p className="inline">@ 16</p>
 		</div>
 	</motion.li>
 );
@@ -37,7 +59,8 @@ const Item4 = () => (
 	<motion.li variants={animations.itemVariants}>
 		<p className="text-slate-500">04</p>
 		<div>
-			<p className="inline">spoke at</p> <Link text="SXSW" href="https://youtu.be/kgsoqvQWfXs" /> <p className="inline">@ 17 about building a nuclear fusion reactor in the garage</p>
+			<p className="inline">published in the</p> <Link text="New York Times" href="https://x.com/AppleFilms/status/1494089759546744832" />{" "}
+			<p className="inline">@ 16, youngest recipient of the Atlanta Review International Poetry Award, + some other awards for writing</p>
 		</div>
 	</motion.li>
 );
@@ -45,7 +68,9 @@ const Item4 = () => (
 const Item5 = () => (
 	<motion.li variants={animations.itemVariants}>
 		<p className="text-slate-500">05</p>
-		immigrated to the U.S. at 7. followed my parent&apos;s american dream to go to a good college. found my best friends there, then left after 10 weeks to pursue a riskier dream.
+		<div>
+			<p className="inline">spoke at</p> <Link text="SXSW" href="https://youtu.be/kgsoqvQWfXs" /> <p className="inline">@ 17 about building a nuclear fusion reactor in the garage</p>
+		</div>
 	</motion.li>
 );
 
