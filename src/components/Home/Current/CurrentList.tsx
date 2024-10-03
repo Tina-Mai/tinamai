ac8dcdbc771761bb398b8dc3e7b068f8f9113bb8
Tina Mai <tinabmai@gmail.com>
2024-10-03 13:38:47 -0700

    current

--- src/components/Home/Current/CurrentList.tsx
+++ src/components/Home/Current/CurrentList.tsx
@@ -15,7 +15,7 @@ const Item1 = () => (
 const Item2 = () => (
 	<motion.li variants={animations.itemVariants}>
 		<p className="text-slate-500">02</p>
-		building & researching human simulating agents and new interfaces to deploy them
+		researching human simulating agents (new architectures inspired by theoretical neuro) & building new interfaces to deploy them
 	</motion.li>
 );
 
